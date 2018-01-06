f949c3d9b73dcb948add5b87dd63a0d6c25f7eb8
liugaoshuai <63352461@qq.com>
2018-01-07 01:29:26 +0800

    dahe

---
+++ server/test.js
@@ -0,0 +1,16 @@
+// 引入编写好的api
+const api = require('./api');
+// 引入文件模块
+const fs = require('fs');
+// 引入处理路径的模块
+const path = require('path');
+// 引入处理post数据的模块
+const bodyParser = require('body-parser');
+// 引入Express
+const express = require('express');
+const app = express();
+const models = require('./db');
+
+// 监听80端口
+app.listen(80);
+console.log('success listen 80');
